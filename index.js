cc01d4c7674bfca9e27856880f26ff2edb723a93
Boniface <Cheruiyotbony@gmail.com>
2023-10-15 10:19:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+const newHeader=document.createElement('newHeader')
+newHeader.setAttribute("id","victory")
+const h1=document.createElement('h1')
+h1.innerText='BONIFACE is the champion'
+newHeader.append(h1)
